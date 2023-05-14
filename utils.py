c3677e15f19e2aaa180050d65551fba3901cf2f2
AnnaToimurzina <2402as@gmail,com>
2023-05-14 22:23:23 +0500

    add utils с функциями

---
+++ utils.py
@@ -0,0 +1,77 @@
+import json
+from datetime import datetime
+import os.path
+
+def read_file(data):
+    if not os.path.isfile(data):
+        raise FileNotFoundError("Файл не найден")
+    try:
+        with open(data, "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except json.JSONDecodeError:
+        raise ValueError("Файл содержит недопустимый формат данных")
+    return data
+
+
+def operations_executed(data):
+    """
+    Возвращает список отфильтрованных операции
+    :param data: список всех операций
+    :return: список опрераций "EXECUTED"
+    """
+    new_list = []
+    for operation in data:
+        if operation.get("state") == "EXECUTED":
+            new_list.append(operation)
+    return new_list
+
+def format_dates(data):
+    '''Сортируем по дате'''
+    data = [trans for trans in data if trans]
+    data = sorted(data, key=lambda x: x["date"], reverse=True)
+    return data
+
+def date_change(data):
+    ''' Преобразует дату перевода в формат ДД.ММ.ГГГГ (пример: 14.10.2018).'''
+    date_changed = datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%f')
+    return date_changed.strftime('%d.%m.%Y')
+
+
+
+def from_user(item):
+    """Номер карты замаскирован и не отображается целиком в формате  XXXX XX** **** XXXX
+    (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом)"""
+    if item is None:
+        return "XXXX XXXX XXXX XXXX"
+    # if we don't have data from where user sent money we print XXXX hidden numbers as example of credit card 16 digits.
+    else:
+        split_string = item.split(' ')
+        account_name = split_string[0]
+        account_number = split_string[1]
+        return f"{account_name} {account_number[0:4]} {account_number[4:6]}{'** ' + ('*' * 4)} {account_number[-4:]}"
+
+
+def to_user(item):
+    """Номер счета замаскирован и не отображается целиком в формате  **XXXX
+       (видны только последние 4 цифры номера счета)"""
+    split_string = item.split(' ')
+    account_name = split_string[0]
+    account_number = split_string[1]
+    return f"{account_name} {'*' * 2}{account_number[-4:]}"
+
+def show_transactions(data):
+    """вывод картинки транзакции """
+    if data is None:
+        return "ERROR 404"
+    for transaction in data[:5]:
+        print("-" * 30)
+        print('{date} {description}\n'
+              '{from_} --> {to}\n'
+              '{amount} {currency}\n'.format(date=date_change(transaction['date']),
+                                             description=transaction['description'],
+                                             from_=from_user(transaction.get('from')),
+                                             to=to_user(transaction['to']),
+                                             amount=transaction['operationAmount']['amount'],
+                                             currency=transaction['operationAmount']['currency']['name'], ))
+
+
