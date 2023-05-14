09399f02905d20538595dc8347c65fc7ce81b357
AnnaToimurzina <2402as@gmail,com>
2023-05-14 22:24:41 +0500

    add main. Программа работает

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import utils
+import os.path
 
+
+json_path = os.path.join('operation.json')
+
+def main():
+    new_peremennaia = utils.read_file(json_path)
+    exe_sorted_by_date = utils.format_dates(new_peremennaia)
+    utils.show_transactions(exe_sorted_by_date)
+
+
+if __name__ == '__main__':
+    main()
 
